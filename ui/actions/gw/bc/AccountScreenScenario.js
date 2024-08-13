039ca9298e6728d487a9d0654f7a883aa65ce109
Vishwajit-Patil <vishwajit.patil@valuemomentum.com>
2024-08-13 22:35:15 +0530

    Added feedback

--- ui/actions/gw/bc/AccountScreenScenario.js
+++ ui/actions/gw/bc/AccountScreenScenario.js
@@ -1,14 +1,14 @@
 import { AccountsTabBar_Ext } from './scenarioPages/navigation/AccountsTabBar_Ext';
-import { AccountScreen_New } from "../../gw/bc/scenarioPages/AccountScreen_New"
+import { AccountGroupMenuLinks_Ext } from './scenarioPages/AccountGroupMenuLinks_Ext.js';
 import { t } from "testcafe";
 import world from "../../../util/gw/world.js"
 
 const accountsTabBar_Ext = new AccountsTabBar_Ext()
-const accountScreen_New = new AccountScreen_New()
+const accountGroupMenuLinks_Ext = new AccountGroupMenuLinks_Ext()
 
 export class AccountScreenScenario {
     async validateAccountScreen(){
-      await t.expect(await accountScreen_New.accountScreenNumber.component.textContent).eql(world.dataMap.get('AccountNumber'))
+      await t.expect(await accountGroupMenuLinks_Ext.accountScreenNumber.component.textContent).eql(world.dataMap.get('AccountNumber'))
 }
   }
 
--- ui/actions/gw/bc/scenarioPages/AccountGroupMenuLinks_Ext.js
+++ ui/actions/gw/bc/scenarioPages/AccountGroupMenuLinks_Ext.js
@@ -1,5 +1,6 @@
 import { PcfButton, PcfCheckBox, PcfComponent, PcfSelectInput } from "@gtui/gt-ui-framework";
+import { AccountGroupMenuLinks } from "../../../../pages/gw/generated/billingsolutions/pages/navigation/menuLinks/AccountGroupMenuLinks";
 
-export class AccountScreen_New {
+export class AccountGroupMenuLinks_Ext extends AccountGroupMenuLinks {
     accountScreenNumber = PcfComponent('#AccountGroup-AccountInfoBar-AccountNumber > div.gw-label.gw-infoValue');
 }
