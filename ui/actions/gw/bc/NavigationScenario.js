1d6207a682104d4c8afc21140caf998465e84870
Mahesh-Bale <mahesh.bale@valuemomentum.com>
2024-08-16 12:36:20 +0530

    corrections done in main branch

--- ui/actions/gw/bc/NavigationScenario.js
+++ ui/actions/gw/bc/NavigationScenario.js
@@ -2,20 +2,16 @@ import { AccountGroupMenuLinks } from '../../../../ui/pages/gw/generated/billing
 import { SearchTabBar } from '../../../pages/gw/generated/billingsolutions/pages/navigation/tabBar/SearchTabBar';
 import { SearchGroupMenuLinks } from '../../../pages/gw/generated/billingsolutions/pages/navigation/menuLinks/SearchGroupMenuLinks';
 import { PolicyGroupMenuLinks } from '../../../pages/gw/generated/billingsolutions/pages/navigation/menuLinks/PolicyGroupMenuLinks';
+import world from "../../../util/gw/world.js"
+import { AccountsTabBar_Ext } from './scenarioPages/navigation/AccountsTabBar_Ext';
 
 const accountGroupMenuLinks = new AccountGroupMenuLinks();
 const searchTabBar = new SearchTabBar()
 const searchGroupMenuLinks = new SearchGroupMenuLinks()
 const policyGroupMenuLinks = new PolicyGroupMenuLinks()
-
-import { AccountsTabBar_Ext } from './scenarioPages/navigation/AccountsTabBar_Ext';
-import world from "../../../util/gw/world.js"
-
-const accountGroupMenuLinks = new AccountGroupMenuLinks();
 const accountsTabBar_Ext = new AccountsTabBar_Ext()
 
 export class NavigationScenario {
-
   async navigateToInvoices() {
     await accountGroupMenuLinks.menuLinksAccountGroup_AccountDetailInvoices.click();
   }
@@ -34,5 +30,5 @@ export class NavigationScenario {
     await accountsTabBar_Ext.accountsTab_ExpandButton.click()
     await accountsTabBar_Ext.accountsTab_AccountNumberSearchInput.setValue(world.dataMap.get('AccountNumber'))
     await accountsTabBar_Ext.accountSearch_Button.click()
-   }
+  }
 }
