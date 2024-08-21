ebc236cea15ba822ddcfe1565d3b29628b6000ad
rasagna-vanga <rasagna.vanga@valuemomentum.com>
2024-08-21 16:01:33 +0530

    SearchInvoives&SomeCleanUp

--- behavior/steps/gw/bc/SearchSteps.js
+++ behavior/steps/gw/bc/SearchSteps.js
@@ -1,18 +1,17 @@
-const { When, Then } = require("@cucumber/cucumber")
-import { World } from "@cucumber/cucumber";
+const { When } = require("@cucumber/cucumber")
 import { NavigationScenario } from "../../../../ui/actions/gw/bc/NavigationScenario";
 import { SearchScenario } from "../../../../ui/actions/gw/bc/SearchScenario";
-import { AccountSummaryScenario } from "../../../../ui/actions/gw/pc/AccountSummaryScenario";
-import { clickTableRecord, searchTableRecord } from "../../../../ui/util/gw/helper";
 import world from "../../../../ui/util/gw/world";
 import { t } from "testcafe"
 
-
 const navigationScenario = new NavigationScenario()
-const accountSummaryScenario = new AccountSummaryScenario()
 const searchScenario = new SearchScenario()
 
 When(/^the user loads the policy with policy number/, async function () {
     await navigationScenario.navigateSearchPolicyScreen()
     await searchScenario.searchWithPolicy(world.dataMap.get('PolicyNumber'))
+});
+
+When(/^the user opens the policy with policy number/, async function () {
+    await navigationScenario.openPolicy(t.ctx.PolicyNumber)
 });
