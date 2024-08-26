2c79938308f03d64c2589dae5188f29e93220351
rasagna-vanga <rasagna.vanga@valuemomentum.com>
2024-08-26 15:25:33 +0530

    AddedFeedback

--- ui/actions/gw/cc/ContactScenario.js
+++ ui/actions/gw/cc/ContactScenario.js
@@ -1,6 +1,6 @@
 import { ClaimMenuLinks_Ext } from './scenarioPages/navigation/menuLinks/ClaimMenuLinks_Ext'
 import { t } from 'testcafe'
-import { navigateAndClickSubmenu, generateRandomStringFunction } from "../../../../ui/util/gw/helper";
+import { navigateAndClickSubmenu, generateRandomStringFunction, clickTableRecord } from "../../../../ui/util/gw/helper";
 import { NewClaimSaved } from "../../../../ui/pages/gw/generated/claimsolutions/pages/other/NewClaimSaved"
 import {ClaimContacts_Ext} from "./scenarioPages/claim/claimPartiesGroup/ClaimContacts_Ext"
 const claimMenuLinks_Ext = new ClaimMenuLinks_Ext()
@@ -13,10 +13,10 @@ export class ContactScenario {
         await newClaimSaved.newClaimSavedDVGoToClaim.click()
         await claimMenuLinks_Ext.partiesInvolved.click()
         await navigateAndClickSubmenu(['Contacts'], '')
-        await claimMenuLinks_Ext.editContactCheckBox.click()
+        await clickTableRecord('_Checkbox_checkboxDiv', "Insured, Main Contact, Reporter")
         await claimMenuLinks_Ext.editButton.click()
         t.ctx.editLastName= generateRandomStringFunction(5)
-        await claimContacts_Ext.claimContactsClaimContactsScreenPeopleInvolvedDetailedListDetailContactBasicsDVPersonNameInputSetGlobalPersonNameInputSetLastName.setValue(await t.ctx.EditContactDetails)
+        await claimContacts_Ext.claimContactsClaimContactsScreenPeopleInvolvedDetailedListDetailContactBasicsDVPersonNameInputSetGlobalPersonNameInputSetLastName.setValue(t.ctx.editLastName)
         await claimMenuLinks_Ext.updateContactDetails.click()
     }
 
--- ui/actions/gw/cc/scenarioPages/navigation/menuLinks/ClaimMenuLinks_Ext.js
+++ ui/actions/gw/cc/scenarioPages/navigation/menuLinks/ClaimMenuLinks_Ext.js
@@ -2,10 +2,8 @@ import { PcfButton, PcfCheckBox, PcfComponent, PcfSelectInput, PcfTextInput } fr
 import { ClaimMenuLinks} from '../../../../../../pages/gw/generated/claimsolutions/pages/navigation/menuLinks/ClaimMenuLinks'
 
 export class ClaimMenuLinks_Ext extends ClaimMenuLinks {
-	partiesInvolved = PcfButton('#Claim-MenuLinks-Claim_ClaimPartiesGroup > div.gw-action--inner');
-	editContactCheckBox = PcfCheckBox('#ClaimContacts-ClaimContactsScreen-PeopleInvolvedDetailedListDetail-PeopleInvolvedDetailedLV-0-_Checkbox_checkboxDiv')
+	partiesInvolved = PcfButton('#Claim-MenuLinks-Claim_ClaimPartiesGroup > div.gw-action--inner')
     editButton = PcfButton('#ClaimContacts-ClaimContactsScreen-PeopleInvolvedDetailedListDetail-ContactBasicsDV_tb-ContactDetailToolbarButtonSet-Edit')
-    lastname = PcfTextInput('#ClaimContacts-ClaimContactsScreen-PeopleInvolvedDetailedListDetail-ContactBasicsDV-PersonNameInputSet-GlobalPersonNameInputSet-LastName')
     updateContactDetails = PcfButton('#ClaimContacts-ClaimContactsScreen-PeopleInvolvedDetailedListDetail-ContactBasicsDV_tb-ContactDetailToolbarButtonSet-Update')
     contactsHeader = PcfComponent('#ClaimContacts-ClaimContactsScreen-ttlBar')
 }
