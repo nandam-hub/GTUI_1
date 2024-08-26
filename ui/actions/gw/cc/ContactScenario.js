fdfb75bc2caa664646e24b015bebbc4fe04d884e
rasagna-vanga <rasagna.vanga@valuemomentum.com>
2024-08-26 16:23:33 +0530

    Added Feedback

--- ui/actions/gw/cc/ContactScenario.js
+++ ui/actions/gw/cc/ContactScenario.js
@@ -1,18 +1,14 @@
 import { ClaimMenuLinks_Ext } from './scenarioPages/navigation/menuLinks/ClaimMenuLinks_Ext'
 import { t } from 'testcafe'
-import { navigateAndClickSubmenu, generateRandomStringFunction, clickTableRecord } from "../../../../ui/util/gw/helper";
-import { NewClaimSaved } from "../../../../ui/pages/gw/generated/claimsolutions/pages/other/NewClaimSaved"
+import { generateRandomStringFunction, clickTableRecord } from "../../../../ui/util/gw/helper";
+
 import {ClaimContacts_Ext} from "./scenarioPages/claim/claimPartiesGroup/ClaimContacts_Ext"
 const claimMenuLinks_Ext = new ClaimMenuLinks_Ext()
-const newClaimSaved = new NewClaimSaved()
 const claimContacts_Ext = new ClaimContacts_Ext()
 
 export class ContactScenario {
 
     async editContact() {
-        await newClaimSaved.newClaimSavedDVGoToClaim.click()
-        await claimMenuLinks_Ext.partiesInvolved.click()
-        await navigateAndClickSubmenu(['Contacts'], '')
         await clickTableRecord('_Checkbox_checkboxDiv', "Insured, Main Contact, Reporter")
         await claimMenuLinks_Ext.editButton.click()
         t.ctx.editLastName= generateRandomStringFunction(5)
