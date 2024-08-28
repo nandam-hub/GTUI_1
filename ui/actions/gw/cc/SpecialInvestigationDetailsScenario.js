5bae632e6e8b5a00a888309ae53c545e4348c02c
Vishwajit-Patil <vishwajit.patil@valuemomentum.com>
2024-08-28 17:43:07 +0530

    updated code for special investigation

--- ui/actions/gw/cc/SpecialInvestigationDetailsScenario.js
+++ ui/actions/gw/cc/SpecialInvestigationDetailsScenario.js
@@ -11,27 +11,21 @@ export class SpecialInvestigationDetailsScenario {
     async updateSIDDetails() {
         await claimMenuLinks.claim_ClaimLossDetailsGroupClaimLossDetailsGroup_SIDetails.click();
         await sIDetails.sIDetailsScreenEdit.click();
-        await this.updateGeneralSIDQuestionnaire(0, 'yes')
-        await this.updateGeneralSIDQuestionnaire(1, 'yes')
-        await this.updateGeneralSIDQuestionnaire(2, 'yes')
-        await this.updateGeneralSIDQuestionnaire(3, 'yes')
-        await this.updateGeneralSIDQuestionnaire(4, 'yes')
-        await this.updateGeneralSIDQuestionnaire(5, 'yes')
-        await this.updateGeneralSIDQuestionnaire(6, 'yes')
-        await this.updateGeneralSIDQuestionnaire(7, 'yes')
-        await this.updateGeneralSIDQuestionnaire(8, 'yes')
-        await this.updateGeneralSIDQuestionnaire(9, 'yes')
-        await this.updateGeneralSIDQuestionnaire(11, 'yes')
-        await this.updateGeneralSIDQuestionnaire(12, 'yes')
-        await this.updateGeneralSIDQuestionnaire(13, 'yes')
+        for (let i = 0; i <= 13; i++) {
+          if (i !== 10) { 
+            await this.updateGeneralSIDQuestionnaire(i, 'yes');
+          }
+        }
         await sIDetails.sIUTotalScoreSIinfo_SIescalateSIU.selectOptionByLabel(world.dataMap.get('Review'));
         await sIDetails.sIUTotalScoreSIinfo_SIEscalateSIUNote.setValue(world.dataMap.get('Reason'));
         await sIDetails.sIDetailsScreenUpdate.click();
     }
+    
     async validateSID() {
-        await t.expect((await returnDataFromTable(6, 2))).contains("Special Investigation")
+        await t.expect((await returnDataFromTable(6, 2))).contains(world.dataMap.get('SID'));
         await t.wait(1000);
     }
+    
     async updateGeneralSIDQuestionnaire(questionNo, radioInput) {
         if (radioInput == 'yes')
             radioInput = 0
