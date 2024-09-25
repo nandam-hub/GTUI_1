090238cf2cea2fb134979f547800c8d732b27e75
ramya <ramya.muralidharan@valuemomentum.com>
2024-09-25 14:14:04 +0530

    added api and repo folder

---
+++ api/actions/pc/producercodes/ProducerCodeDO.java
@@ -0,0 +1,21 @@
+package pc.producercodes;
+
+public class ProducerCodeDO {
+
+    private final String code;
+    private final String id;
+
+    public ProducerCodeDO(String code, String id) {
+        this.code = code;
+        this.id = id;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+}
---
+++ api/karate-config.js
@@ -0,0 +1,20 @@
+function fn() {
+    //Read base configurations
+    var configFun = read('classpath:gw/gtapi/util/testsconfig/gw-config-utils.js');
+    var config = configFun();
+
+    config.policyDataContainer = Java.type('pc.PolicyDataContainer');
+
+    //Configure AUT URLs
+    config.ccBaseUrl = java.lang.System.getenv('ccBaseUrl') ? java.lang.System.getenv('ccBaseUrl') : 'DEFAULT_CC_URL';
+    config.pcBaseUrl = java.lang.System.getenv('pcBaseUrl') ? java.lang.System.getenv('pcBaseUrl') : 'DEFAULT_PC_URL';
+
+    //AUT Instance Healthcheck
+    appHealthCheck(config.ccBaseUrl, true);
+    appHealthCheck(config.pcBaseUrl, true);
+
+    //Set Basic Auth Header credentials
+    config.credentialsUtil.setCredentialsForAuthHeader('su','gw');
+
+    return config;
+}
